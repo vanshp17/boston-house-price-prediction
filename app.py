80cf5bb98a99f1cb72aad06da672256be3ea9f94
= <patilvanshita1703@gmail.com>
2023-12-01 15:03:27 +0530

    This is the commit for web application

--- app.py
+++ app.py
@@ -24,5 +24,22 @@ def predict_api():
     print(result[0])
     return jsonify(result[0])
 
+
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    data =[float(x) for x in request.form.values()]
+    final_input = scaler.transform(np.array(data).reshape(1,-1))
+    print(final_input)
+
+    output = model.predict(final_input)[0]
+    return render_template('home.html',prediction_text = "The House Price prediction is {}".format(output))
+
+
+
+
+
+
+
 if __name__ == '__main__':
     app.run(debug=True)
